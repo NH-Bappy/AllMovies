08d5a97333f0a78acfc5dbd04903c828b1f776e6
NH-Bappy <naimulbappy2207@gmail.com>
2025-04-12 23:50:34 +0600

    add some function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 
+import Home from '../pages/Home'
 import './App.css'
-import MoviesCard from './components/MoviesCard'
+
 
 function App() {
 
-  const movieNumber = 1;
+
 
   return (
     <>
-    <MoviesCard movie={{title: "300" ,release_date: "2010"}}/>
+    <Home/>
     </>
   )
 }
